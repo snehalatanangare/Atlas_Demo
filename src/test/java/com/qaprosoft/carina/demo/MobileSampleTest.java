d79076e55bbef99be2a757372af82f5e96583a25
Nikita Shinde <Q4310@Corp.qualitykiosk.com>
2019-11-21 18:57:04 +0530

    capabilites included

--- src/test/java/com/qaprosoft/carina/demo/MobileSampleTest.java
+++ src/test/java/com/qaprosoft/carina/demo/MobileSampleTest.java
@@ -1,11 +1,17 @@
 package com.qaprosoft.carina.demo;
 
+import java.io.File;
+
 import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.qaprosoft.carina.core.foundation.AbstractTest;
-import com.qaprosoft.carina.core.foundation.utils.mobile.MobileUtils;
+import com.qaprosoft.carina.core.foundation.utils.Configuration;
+import com.qaprosoft.carina.core.foundation.utils.R;
+import com.qaprosoft.carina.core.foundation.utils.factory.DeviceType.Type;
 import com.qaprosoft.carina.core.foundation.utils.ownership.MethodOwner;
 import com.qaprosoft.carina.demo.mobile.gui.pages.common.CarinaDescriptionPageBase;
 import com.qaprosoft.carina.demo.mobile.gui.pages.common.ContactUsPageBase;
@@ -17,11 +23,30 @@ import com.qaprosoft.carina.demo.utils.MobileContextUtils;
 import com.qaprosoft.carina.demo.utils.MobileContextUtils.View;
 
 
+
 public class MobileSampleTest extends AbstractTest {
 
+	@BeforeMethod
+	public void setCustCaps(){
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("Capability_Username","sushem.bhagat@qualitykiosk.com");
+		capabilities.setCapability("Capability_ApiKey","s5w896dptxmy7qr239ypdn73");
+		capabilities.setCapability("Capability_ApplicationName", "carinademoexample.apk");
+		capabilities.setCapability("Capability_DurationInMinutes", 60);
+		capabilities.setCapability("Capability_DeviceFullName","Samsung_GalaxyJ6_Android_9.0.0");
+		capabilities.setCapability("deviceType","phone");
+		capabilities.setCapability("platformName","ANDROID");
+		capabilities.setCapability("automationName","uiautomator2");
+		capabilities.setCapability("appActivity","com.solvd.carinademoapplication.ActivitySplash");
+		capabilities.setCapability("appPackage","com.solvd.carinademoapplication");
+		R.CONFIG.getProperties().setProperty("deviceName", "Samsung_GalaxyJ6_Android_9.0.0");
+		getDriver("default", capabilities, R.CONFIG.get("selenium_host"));
+	}
+	
     @Test(description = "JIRA#DEMO-0011")
     @MethodOwner(owner = "qpsdemo")
     public void testLoginUser() {
+    	setApplicationPath();
         String username = "Test user";
         String password = RandomStringUtils.randomAlphabetic(10);
         WelcomePageBase welcomePage = initPage(getDriver(), WelcomePageBase.class);
@@ -36,7 +61,7 @@ public class MobileSampleTest extends AbstractTest {
         Assert.assertTrue(carinaDescriptionPage.isPageOpened(), "Carina description page isn't opened");
     }
 
-    @SuppressWarnings("deprecation")
+    /*@SuppressWarnings("deprecation")
 	@Test(description = "JIRA#DEMO-0011")
     @MethodOwner(owner = "qpsdemo")
     public void testWebView() {
@@ -50,7 +75,6 @@ public class MobileSampleTest extends AbstractTest {
         contactUsPage.typeName("John Doe");
         contactUsPage.typeEmail("some@email.com");
         contactUsPage.typeQuestion("This is a message");
-        MobileUtils.hideKeyboard();
         contactUsPage.submit();
         Assert.assertTrue(contactUsPage.isSuccessMessagePresent() || contactUsPage.isRecaptchaPresent(),
             "message was not sent or captcha was not displayed");
@@ -79,6 +103,17 @@ public class MobileSampleTest extends AbstractTest {
         Assert.assertTrue(uiElements.isFemaleRadioButtonSelected(), "Female radio button was not selected!");
         uiElements.clickOnOtherRadioButton();
         Assert.assertTrue(uiElements.isOthersRadioButtonSelected(), "Others radio button was not selected!");
-    }
+    }*/
 
+    private void setApplicationPath() {
+		String pathToApp;
+		String iosPath = "src/main/resources/carinademoexample.zip";
+		String androidPath = "src/main/resources/carinademoexample.apk";
+		if (Type.ANDROID_PHONE.getFamily().equalsIgnoreCase(R.CONFIG.get("capabilities.platformName")))
+			pathToApp = androidPath;
+		else 
+			pathToApp = iosPath;
+		File file = new File(pathToApp);
+		Configuration.setMobileApp(file.getAbsolutePath());
+	}
 }
